c7419a6a2264ab7292a0683c807a7467afeced00
Christopher N. Hesse <raymanfx@gmail.com>
2018-11-26 19:28:54 +0100

    CPH1859: Hexedit libmtk-ril to fix incoming calls
    Taken from: https://github.com/phhusson/treble_experimentations/issues/57#issuecomment-416998086
    
    Change-Id: Ie642668898e1b93672f360a9daf712fc43c11941
    Signed-off-by: Christopher N. Hesse <raymanfx@gmail.com>
    Signed-off-by: Debayan Kar <kardebayan3@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -53,6 +53,14 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib64/libmtk-ril.so)
+            sed -i 's|AT+EAIC=2|AT+EAIC=3|g' "${2}"
+            ;;
+    esac
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
