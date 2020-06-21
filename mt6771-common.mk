c723d9f3e9b669ac36ce43701c2014b95024da5c
Kshitij Gupta <kshitijgm@gmail.com>
2020-06-21 12:40:27 +0000

    mt6771-common: Set MTK services log level
    Change-Id: I44eafc066c34e0e9076849325823d6d6a5d0421a
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- mt6771-common.mk
+++ mt6771-common.mk
@@ -296,3 +296,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+
+# Properties
+-include $(LOCAL_PATH)/configs/mtk_services_log.mk
