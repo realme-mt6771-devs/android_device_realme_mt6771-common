6f4c89d073c2eb60b3d2ddafd261bd951c8b83f8
luk1337 <priv.luk@gmail.com>
2020-09-10 03:57:15 +0200

    CPH1859: Build libhidltransport and libhwbinder
    Change-Id: I0fdbdc04169d1ab7848eb936fd86b284d288dc9d

--- device.mk
+++ device.mk
@@ -59,6 +59,10 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor \
     libunwindstack.vendor
 
 # Keylayout
