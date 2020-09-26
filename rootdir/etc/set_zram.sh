1710ce71288ea150b44ee4380cc4c938c88fc1e2
devil-black-786 <fahimahmad904@gmail.com>
2020-09-26 08:29:08 +0000

    mt6771-common: tune zram comp-streams and and page cluster
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- rootdir/etc/set_zram.sh
+++ rootdir/etc/set_zram.sh
@@ -15,18 +15,24 @@ elif [ $MemTotal -le 3145728 ]; then
   echo 2147483648 > /sys/block/zram0/disksize
   echo 160 > /proc/sys/vm/swappiness
   echo 60 > /proc/sys/vm/direct_swappiness
+  echo 8 > /sys/block/zram0/max_comp_streams
+  echo 0 > /proc/sys/vm/page-cluster
 elif [ $MemTotal -le 4194304 ]; then
   #config 2GB zram size with memory less than 4 GB
   echo lz4 > /sys/block/zram0/comp_algorithm
   echo 2147483648 > /sys/block/zram0/disksize
   echo 160 > /proc/sys/vm/swappiness
   echo 60 > /proc/sys/vm/direct_swappiness
+  echo 8 > /sys/block/zram0/max_comp_streams
+  echo 0 > /proc/sys/vm/page-cluster
 elif [ $MemTotal -le 6291456 ]; then
   #config 1GB zram size with memory 6 GB
   echo lz4 > /sys/block/zram0/comp_algorithm
   echo 2415919104 > /sys/block/zram0/disksize
   echo 160 > /proc/sys/vm/swappiness
   echo 60 > /proc/sys/vm/direct_swappiness
+  echo 8 > /sys/block/zram0/max_comp_streams
+  echo 0 > /proc/sys/vm/page-cluster
 else
   #config 1GB zram size with memory greater than 6 GB
   echo lz4 > /sys/block/zram0/comp_algorithm
