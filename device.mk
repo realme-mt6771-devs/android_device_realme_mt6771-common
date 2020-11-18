e679bea9bb167a201e2b715864c5f5a62904bc73
Peter Cai <peter@typeblog.net>
2020-11-18 19:21:13 +0530

    CPH1859: dirty hack to make IMS work for subsequent reboots
    * I have no idea why MTK IMS stops working after a reboot when SELinux
      is set to enforcing. I have literally tried to eliminate all denials
      but the bug was still present, and toggling SELinux to permissive
      magically fixes everything.
    * Manually re-enabling VoLTE fixes the problem, so that's basically what
      I'm doing here -- to automate the process.
    * It can't be guaranteed to always work and we should still try to
      figure out why this happens at all. But for now, this is at least
      better than putting the whole device to permissive.
    * Also, we should probably stop messing with these blobs and start
      looking into reverse-engineered solutions like MtkIms instead.
    
    Change-Id: I5fc4301271f6f3850d667be695254e6bbad30da8
    Signed-off-by: Ayush <ayush@disroot.org>

--- device.mk
+++ device.mk
@@ -73,6 +73,9 @@ PRODUCT_PACKAGES += \
     libunwindstack.vendor
 
 # IMS
+PRODUCT_PACKAGES += \
+    ImsInit
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
 
