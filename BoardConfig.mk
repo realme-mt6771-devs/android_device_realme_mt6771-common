68427519a8e1886b9da0ecad777c23d4621c4059
Aayush Gupta <aayushgupta219@gmail.com>
2021-01-19 11:27:45 +0530

    CPH1859: Label light HIDL and grant required permissions
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: Ifc68e9ee59f9ca259b4e88ff382373900e1c455d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,5 +93,7 @@ VENDOR_SECURITY_PATCH := 2020-05-05
 # SEPolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
 
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
 # VNDK
 BOARD_VNDK_VERSION := current
