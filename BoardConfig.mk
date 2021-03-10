2641746299671c2aa30960e81f3756143ecdcc5a
Bruno Martins <bgcngm@gmail.com>
2021-03-10 09:40:46 +0600

    CPH1859: Use MIUI poweroff charging animation from merlin
    * From Redmi/merlin/merlin:11/RP1A.200720.011/V12.5.1.0.RJOMIXM:user/release-keys
    
    Change-Id: Ibbfda5b53b195c27ea0667a0a77be84787d999ed

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,6 +94,7 @@ VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 # SEPolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
 
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # VNDK
