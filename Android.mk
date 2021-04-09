3020ee9201eeb64c64524c5db01f4dba860ab677
TheMalachite <eliasgheeraert@gmail.com>
2021-04-09 23:10:56 +0530

    CPH1859: Add blobs symlinks
    - Based over https://github.com/GS290-dev/android_device_gigaset_GS290/commit/348b6ac634c5f2f2df2fd9ab27703353f62e9df5
    
    [@kardebayan: Adapt symlinks for oppo6771_17065]
    
    Change-Id: Ib4e92ac73cceee88b73f83b6f66cbb2182264b9a
    Co-authored-by: Debayan <kardebayan3@gmail.com>
    Co-authored-by: Erfan Abdi <erfangplus@gmail.com>

--- Android.mk
+++ Android.mk
@@ -9,4 +9,48 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),CPH1859)
   subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+
+include $(CLEAR_VARS)
+
+GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.default.so
+GATEKEEPER_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.default.so
+$(GATEKEEPER_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf libSoftGatekeeper.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(GATEKEEPER_SYMLINK)
+
+GATEKEEPER_DEVICE_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.mt6771.so
+GATEKEEPER_DEVICE_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.mt6771.so
+$(GATEKEEPER_DEVICE_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf libMcGatekeeper.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(GATEKEEPER_DEVICE_SYMLINK)
+
+KEYSTORE_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/keystore.mt6771.so
+KEYSTORE_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/keystore.mt6771.so
+$(KEYSTORE_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf libMcTeeKeymaster.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(KEYSTORE_SYMLINK)
+
+KMSETKEY_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/kmsetkey.default.so
+KMSETKEY_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/kmsetkey.default.so
+$(KMSETKEY_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf kmsetkey.trustonic.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(KMSETKEY_SYMLINK)
+
+VULKAN_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/vulkan.mt6771.so
+VULKAN_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/vulkan.mt6771.so
+$(VULKAN_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf ../egl/libGLES_mali.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(VULKAN_SYMLINK)
+
 endif
+
