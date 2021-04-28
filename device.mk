345762f7154f926e3308e86de70d734bcfec549f
Erfan Abdi <erfangplus@gmail.com>
2021-04-28 03:54:57 +0200

    CPH1859: Cleanup rootdir scripts
    Change-Id: Icc33a6561e43fb699f9f80b455be9f20bc9db385

--- device.mk
+++ device.mk
@@ -173,22 +173,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     engineer_vendor_shell.sh \
     init.oppo.securetemplet.sh \
-    factory_init.connectivity.rc \
-    factory_init.project.rc \
-    factory_init.rc \
-    init.ago.rc \
     init.connectivity.rc \
     init.modem.rc \
     init.mt6771.rc \
     init.mt6771.usb.rc \
     init.oppo.reserve.rc \
-    init.project.rc \
     init.sensor_1_0.rc \
-    meta_init.connectivity.rc \
-    meta_init.modem.rc \
-    meta_init.project.rc \
-    meta_init.rc \
-    multi_init.rc \
     fstab.mt6771 \
     ueventd.mt6771.rc
 
