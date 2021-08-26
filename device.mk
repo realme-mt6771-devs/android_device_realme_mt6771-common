52b4a0e28dc5bf569b1c9715e75abd96f3b86f3f
Debayan <kardebayan3@gmail.com>
2021-08-26 15:17:38 +0530

    CPH1859: Import seccomp from stock
    * From full_oppo6771_17065-user 9 PPR1.180610.011 eng.root.20200509.060522 release-keys
    
    Change-Id: I2da2f43d0e645eff58eefa3e9460d6f4636f959d
    Signed-off-by: Debayan <kardebayan3@gmail.com>

--- device.mk
+++ device.mk
@@ -61,6 +61,12 @@ PRODUCT_PACKAGES += \
     fstab.mt6771 \
     ueventd.mt6771.rc
 
+# Seccomp
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/seccomp/configstore@1.1.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/configstore@1.1.policy \
+    $(LOCAL_PATH)/configs/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/configs/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+
 # WiFi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
