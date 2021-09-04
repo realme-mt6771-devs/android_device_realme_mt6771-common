c007cf2c2aee6a7a979bf0bcc70008676b70f95c
Debayan Kar <kardebayan3@gmail.com>
2021-09-04 23:14:15 +0530

    CPH1859: Inherit proprietary blobs
    * Also track our vendor repo while we are at it
    
    Change-Id: I8b05a6fe38db4b6661fc97906e7ad79532c935e0

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/oppo/CPH1859/CPH1859-vendor.mk)
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@4.0 \
