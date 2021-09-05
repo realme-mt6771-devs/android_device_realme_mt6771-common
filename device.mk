b0f13cd18be4370af9aabe29b75197686f665df3
Debayan Kar <kardebayan3@gmail.com>
2021-09-06 00:54:38 +0530

    CPH1859: Build android.hardware.camera.provider@2.4
    * Fixes :-
    
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/camerahalserver": library "android.hardware.camera.provider@2.4.so" not found: needed by main executable
    
    Change-Id: Id6ee161457a66c91d3f919f79a3997efe448060e
    Signed-off-by: Debayan Kar <kardebayan3@gmail.com>

--- device.mk
+++ device.mk
@@ -36,6 +36,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4
+
 # Display
 PRODUCT_PACKAGES += \
     libvulkan
