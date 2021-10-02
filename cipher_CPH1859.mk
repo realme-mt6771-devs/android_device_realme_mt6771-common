28044563e0ff73f5d37ba19f99494ab794855aac
maitreya29 <maitreyapatni30@gmail.com>
2021-10-03 00:24:52 +0530

    CPH1859: Inherit every AOSP config that exists
    Change-Id: I0e2f6692492ff6111e88a37b90381a4c9c5df77e

--- cipher_CPH1859.mk
+++ cipher_CPH1859.mk
@@ -8,6 +8,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_product.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
 # Inherit from CPH1859 device
 $(call inherit-product, device/oppo/CPH1859/device.mk)
