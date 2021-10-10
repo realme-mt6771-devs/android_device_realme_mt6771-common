68d390f256a4059da7c8a55d53a35c26fe51771d
Debayan Kar <kardebayan3@gmail.com>
2021-10-10 16:48:10 +0530

    CPH1859: Build vendor variant of android.hardware.camera.provider@2.4
    * Fixes :-
    
    - F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/camerahalserver": library "android.hardware.camera.provider@2.4.so" not found: needed by main executable
    
    Change-Id: Icbd66662472b885c46e860020aede0271cb3a45c

--- device.mk
+++ device.mk
@@ -45,7 +45,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4
+    android.hardware.camera.provider@2.4.vendor
 
 PRODUCT_PACKAGES += \
     Snap
