7065d56066188cf5095a2c15182ba0ab8b999e2c
Sakthivel Nadar <55133980+SparXFusion@users.noreply.github.com>
2021-11-25 12:07:18 +0530

    mt6771-common: build more vendor sided camera blobs
    Fixes: 
    
    11-25 05:16:42.355   940   940 E mtkcam-devicemgr: [loadDeviceFactory] dlopen: libmtkcam_device3.so error=dlopen failed: library "android.hardware.camera.device@3.3.so"
    11-25 05:16:43.068   940   940 E vndksupport: Could not load /vendor/lib64/hw/vendor.mediatek.hardware.camera.ccap@1.0-impl.so from default namespace: dlopen failed: library "android.hardware.camera.device@3.3.so" not found: needed by /vendor/lib64/hw/vendor.mediatek.hardware.camera.ccap@1.0-impl.so in namespace (default).
    11-25 05:16:43.095   940   940 E vndksupport: Could not load /vendor/lib64/hw/vendor.mediatek.hardware.camera.camdump@1.0-impl.so from default namespace: dlopen failed: library "android.hardware.camera.device@3.4.so" not found: needed by /vendor/lib64/hw/vendor.mediatek.hardware.camera.camdump@1.0-impl.so in namespace (default).
    
    
    Co-authored-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- mt6771-common.mk
+++ mt6771-common.mk
@@ -43,6 +43,12 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4.vendor \
+    android.hardware.camera.device@3.2.vendor \
+    android.hardware.camera.device@3.3.vendor \
+    android.hardware.camera.device@3.4.vendor \
+    android.hardware.camera.device@3.5.vendor \
+    android.hardware.camera.provider@2.4 \
     android.hardware.camera.provider@2.4.vendor
 
 PRODUCT_PACKAGES += \
