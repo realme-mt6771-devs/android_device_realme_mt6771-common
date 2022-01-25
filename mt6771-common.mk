6ff86fdead2138b86ad6c52cdccb0c6ed8d3951d
SparXFusion <s2234nadar@gmail.com>
2022-01-25 15:09:23 +0000

    mt6771-common: build vendor variant of libtinycompress
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- mt6771-common.mk
+++ mt6771-common.mk
@@ -20,6 +20,7 @@ PRODUCT_PACKAGES += \
     audio.usb.default \
     libaudiopreprocessing \
     libtinycompress \
+    libtinycompress.vendor \
     libavservices_minijail \
     libavservices_minijail.vendor \
     libavservices_minijail_vendor \
